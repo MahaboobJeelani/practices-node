5444f2b11b22bf869dbc9dd581df6edb0bce2e16
Shaik Mahaboob Jeelani Basha <149247472+MahaboobJeelani@users.noreply.github.com>
2024-07-09 12:20:07 +0530

    Update Socketio.js

--- Socketio/Socketio.js
+++ Socketio/Socketio.js
@@ -1,39 +1,87 @@
+// const express = require('express');
+// const http = require('http');
+// const path = require('path');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Socket.io;
+// io.on("connection", (socket) => {
+//     console.log("User Connected");
+
+//     socket.on('joinRoom', (room) => {
+//         socket.join(room);
+//         console.log(`User joined room: ${room}`);
+//         socket.to(room).emit('message', `User has joined room: ${room}`);
+//     });
+
+//     socket.on('leaveRoom', (room) => {
+//         socket.leave(room);
+//         console.log(`User left room: ${room}`);
+//         socket.to(room).emit('message', `User has left room: ${room}`);
+//     });
+
+//     socket.on('user-message', (data) => {
+//         const { room, message } = data;
+//         io.to(room).emit('message', message);
+//     });
+// });
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// server.listen(9000, () => {
+//     console.log(`Server is running on port 9000`);
+// });
+
+
+
+
 const express = require('express')
 const http = require('http')
 const path = require('path')
 const { Server } = require('socket.io')
 
 const app = express()
-const server = http.createServer(app);
-const io = new Server(server)
+const server = http.createServer(app)
+const io = new Server(server);
 
-app.use(express.static(path.resolve('./public')))
+app.use(express.static(path.resolve(__dirname, 'public')))
 
-// Socket.io
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
 
-    socket.on('joinRoom', (room) => {
-        socket.join(room);
-        console.log(`User joined room: ${room}`);
-        socket.to(room).emit('message', `User has joined room: ${room}`);
-    });
-
+    socket.on('joinRoom', room => {
+        socket.join(room)
+        console.log(room, socket.id);
+        socket.to(room).emit('roomJoined', `User has joined Room ${room}`)
+    })
 
     socket.on('leaveRoom', (room) => {
         socket.leave(room);
-        console.log(`User left room: ${room}`);
+        console.log(`User has left room: ${room}`);
         socket.to(room).emit('message', `User has left room: ${room}`);
     });
-    
-    socket.on('user-message', (message) => {
-        io.emit('message', message)
-    })
+
+    socket.on('message', ({ room, message }) => {
+        io.to(room).emit('message', message);
+    });
+
+    // socket.on('message', message => {
+    //     io.emit('sendMessage', message)
+    // })
 })
 
 app.get('/', (req, resp) => {
-    return resp.sendFile('/public/index.html')
+    resp.sendFile(`/public/index.html`)
 })
 
-server.listen(9000, () => {
-    console.log(`Server is running on the port 9000`);
-});
+
+server.listen(8081, () => {
+    console.log('server is running on the port 8081');
+})
+
